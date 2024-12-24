1e3cba4dbc7bb33fc29b80873044a3dc3a8afb7a
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2024-12-24 15:25:56 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         cacheProvider: false,
         providerOptions,
       });
-
+// modal instance
       const web3ModalInstance = await web3modal.connect();
       const web3ModalProvider = new BrowserProvider(web3ModalInstance);
 
